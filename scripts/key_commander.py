b46425be48b3fe585001b65b330421673abab1d1
Jorimica <48402945+Jorimica@users.noreply.github.com>
2019-06-05 23:26:57 -0500

    Update key_commander.py

--- scripts/key_commander.py
+++ scripts/key_commander.py
@@ -7,9 +7,12 @@ from std_msgs.msg import String
 from std_msgs.msg import Empty
 from std_msgs.msg import Int8
 
-from getkey import getkeys, keys
+import getkey
 # if the system turn a error about getkey, use the following command in a shell to install the extension
 # 'sudo pip install getkey'
+# To prevent errors copy the output of the next print and go to that directory. Then open __init__.py and change 
+# 'getkey = __platform.getkey' to 'getkeys = __platform.getkey'
+print(getkey.__file__)
 
 rospy.init_node('key_commander', anonymous=True)
 
@@ -29,8 +32,8 @@ CTRL-C to quit
 #main
 
 pub_start = rospy.Publisher("turtlebot_start", Int8, queue_size=10)
-pub_takeoff = rospy.Publisher("ardrone/takeoff", Empty, queue_size=10)
-pub_land = rospy.Publisher("ardrone/land", Empty, queue_size=10)
+pub_takeoff = rospy.Publisher("/bebop/takeoff", Empty, queue_size=10)
+pub_land = rospy.Publisher("/bebop/land", Empty, queue_size=10)
 
 print msg
 
@@ -45,16 +48,12 @@ while not rospy.is_shutdown():
 	
 	if key == 'q':
 		#pub_start.publish(1)
-		print "esta es q"
 
 	elif key == 'a':
 		#pub_takeoff.publish(Empty())
-		print "esta es a"
-
 
 	elif key == 's':
 		#pub_land.publish(Empty())
-		print "esta es s"
 
 	rospy.sleep(0.1)
 
